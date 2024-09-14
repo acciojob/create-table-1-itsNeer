723a84764e6465f75491eb1a32192397b3c6bb9e
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-09-14 12:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table= document.querySelector("#sampleTable")
+	let newRow= table.insertRow(0)
+	let cell1= newRow.insertCell()
+	let cell2= newRow.insertCell()
+  cell1.innerText="New Cell1"
+	cell2.innerText = "New Cell2"
 }
